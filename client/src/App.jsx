aa92cbda0c37fd42408c87777756c9f2e35495a7
GaneshKumarSeepana <seepanaganeshkumar@gmail.com>
2026-02-19 11:30:09 +0530

    Initial frontend setup with Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black min-h-screen text-white">
+      <h1 className="text-4xl font-bold text-center pt-10">
+        YouTube Clone 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
